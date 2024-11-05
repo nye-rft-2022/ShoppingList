5c4d3c37327aa76a8f8711d1db18fae58ad3b1de
csbala <50990890+csbala@users.noreply.github.com>
2024-11-05 15:41:25 +0100

    update

--- apitest/src/test/java/com/epam/apitest/APITest.java
+++ apitest/src/test/java/com/epam/apitest/APITest.java
@@ -14,102 +14,102 @@ import static org.hamcrest.Matchers.hasKey;
 
 public class APITest {
 
-    private final String BASE_URL = "http://localhost:8080";
+        private final String BASE_URL = "http://localhost:8080";
 
-    @Test
-    public void When_UsersListIsRequested_Expect_StatusOk() {
-        Response response = request("get", BASE_URL + "/user/users");
+        @Test
+        public void When_UsersListIsRequested_Expect_StatusOk() {
+                Response response = request("get", BASE_URL + "/user/users");
 
-        response.then()
-                .statusCode(200);
-    }
+                response.then()
+                                .statusCode(200);
+        }
 
-    @Test
-    public void When_UsersListIsRequested_Expect_ItsNotEmpty() {
-        Response response = request("get", BASE_URL + "/user/users");
+        @Test
+        public void When_UsersListIsRequested_Expect_ItsNotEmpty() {
+                Response response = request("get", BASE_URL + "/user/users");
 
-        response.then()
-                .body("size()", is(not(empty())));
-    }
+                response.then()
+                                .body("size()", is(not(empty())));
+        }
 
-    @Test
-    public void When_UsersListIsRequested_Expect_ProperFields() {
-        Response response = request("get", BASE_URL + "/user/users");
+        @Test
+        public void When_UsersListIsRequested_Expect_ProperFields() {
+                Response response = request("get", BASE_URL + "/user/users");
 
-        response.then()
-                .body("[0]", hasKey("id"))
-                .body("[0]", hasKey("userName"))
-                .body("[0]", hasKey("shoppingList"))
-                .body("[0].shoppingList", hasKey("name"))
-                .body("[0].shoppingList", hasKey("name"));
-    }
+                response.then()
+                                .body("[0]", hasKey("id"))
+                                .body("[0]", hasKey("userName"))
+                                .body("[0]", hasKey("shoppingList"))
+                                .body("[0].shoppingList", hasKey("name"))
+                                .body("[0].shoppingList", hasKey("name"));
+        }
 
-    @Test
-    public void When_UserIsRequested_Expect_StatusOk() {
-        String userId = given()
-                .when()
-                .baseUri(BASE_URL + "/user/users")
-                .when()
-                .get()
-                .body()
-                .path("[0].id")
-                .toString();
+        @Test
+        public void When_UserIsRequested_Expect_StatusOk() {
+                String userId = given()
+                                .when()
+                                .baseUri(BASE_URL + "/user/users")
+                                .when()
+                                .get()
+                                .body()
+                                .path("[0].id")
+                                .toString();
 
-        Response response = request("get", BASE_URL + "/user/" + userId);
+                Response response = request("get", BASE_URL + "/user/" + userId);
 
-        response.then()
-                .statusCode(200);
-    }
+                response.then()
+                                .statusCode(200);
+        }
 
-    @Test
-    public void When_UserIsRequested_Expect_ProperFields() {
-        String userId = given()
-                .when()
-                .baseUri(BASE_URL + "/user/users")
-                .when()
-                .get()
-                .body()
-                .path("[0].id")
-                .toString();
+        @Test
+        public void When_UserIsRequested_Expect_ProperFields() {
+                String userId = given()
+                                .when()
+                                .baseUri(BASE_URL + "/user/users")
+                                .when()
+                                .get()
+                                .body()
+                                .path("[0].id")
+                                .toString();
 
-        Response response = request("get", BASE_URL + "/user/" + userId);
+                Response response = request("get", BASE_URL + "/user/" + userId);
 
-        response.then()
-                .body("", hasKey("id"))
-                .body("", hasKey("userName"))
-                .body("", hasKey("shoppingList"))
-                .body("shoppingList", hasKey("name"))
-                .body("shoppingList", hasKey("entries"));
-    }
+                response.then()
+                                .body("", hasKey("id"))
+                                .body("", hasKey("userName"))
+                                .body("", hasKey("shoppingList"))
+                                .body("shoppingList", hasKey("name"))
+                                .body("shoppingList", hasKey("entries"));
+        }
 
-    @Test
-    public void When_InvalidUserIsRequested_Expect_ErrorResponse() {
-        Response response = request("get", BASE_URL + "/user/notValid");
+        @Test
+        public void When_InvalidUserIsRequested_Expect_ErrorResponse() {
+                Response response = request("get", BASE_URL + "/user/notValid");
 
-        response.then()
-                .statusCode(400);
-    }
+                response.then()
+                                .statusCode(400);
+        }
 
-    @Test
-    public void When_NonExistingUserIsRequested_Expect_ErrorResponse() {
-        Response response = request("get", BASE_URL + "/user/500");
+        @Test
+        public void When_NonExistingUserIsRequested_Expect_ErrorResponse() {
+                Response response = request("get", BASE_URL + "/user/500");
 
-        response.then()
-                .statusCode(404);
-    }
+                response.then()
+                                .statusCode(404);
+        }
 
-    @Test
-    public void When_CreatingNewUserWithAlreadyExistingEmail_ErrorResponse() {
-        RequestSpecification request = given().log().ifValidationFails();
+        @Test
+        public void When_CreatingNewUserWithAlreadyExistingEmail_ErrorResponse() {
+                RequestSpecification request = given().log().ifValidationFails();
 
-        File jsonDataInFile = new File("apitest/src/test/resources/testUser2.json");
+                File jsonDataInFile = new File(getClass().getClassLoader().getResource("testUser2.json").getFile());
 
-        request.header("Content-Type", "application/json");
-        request.body(jsonDataInFile);
+                request.header("Content-Type", "application/json");
+                request.body(jsonDataInFile);
 
-        Response response = request.request("post", BASE_URL + "/user/add");
+                Response response = request.request("post", BASE_URL + "/user/add");
 
-        response.then()
-                .statusCode(400);
-    }
+                response.then()
+                                .statusCode(400);
+        }
 }
